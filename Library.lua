486f571625cec9449fcb73877150df12d2c030d9
Curd <68171186+Relidium@users.noreply.github.com>
2022-09-01 13:18:24 -0400

    Testing groupbox textlabel position

--- Library.lua
+++ Library.lua
@@ -1007,7 +1007,8 @@ do
 
         local TextLabel = Library:CreateLabel({
             Size = UDim2.new(1, -4, 0, 15);
-            TextSize = 14;
+            Position = UDim2.new(0, 0, 0, -3);
+            TextSize = 13;
             Text = Text;
             TextWrapped = DoesWrap or false,
             RichText = true,
