5d61a4352fd2568f04b328efb7b52ab36974c6b0
Curd <68171186+Relidium@users.noreply.github.com>
2022-09-01 12:15:11 -0400

    They call me commitspammer

--- Library.lua
+++ Library.lua
@@ -2120,7 +2120,7 @@ do
 
     local WatermarkOuter = Library:Create('Frame', {
         BorderColor3 = Color3.new(0, 0, 0);
-        Position = UDim2.new(0, 400, 0, -30);
+        Position = UDim2.new(0, 600, 0, 450);
         Size = UDim2.new(0, 213, 0, 20);
         ZIndex = 200;
         Visible = false;
